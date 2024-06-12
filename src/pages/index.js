0d220cc1b63e57b78f954018da582a655d231750
kihno <stistyle13@gmail.com>
2024-06-11 17:06:04 -0700

    Create animated hire me button

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import profilePic from "../../website images/csm.png"
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/components/Icons'
+import HireMe from '@/components/HireMe'
+import lightbulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
 
 export default function Home() {
   return (
@@ -44,6 +46,10 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+        <HireMe />
+        <div className='absolute right-8 bottom-8 inline-block w-24'>
+          <Image src={lightbulb} alt="kihno" className='w-full h-auto' />
+        </div>
       </main>
     </>
   )
