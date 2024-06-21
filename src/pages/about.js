4e73ff0bebb32d2a015c76fc6e55b6e6ee4f6465
kihno <stistyle13@gmail.com>
2024-06-21 16:35:52 -0700

    Create skills component

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 import profilePic from "../../public/images/profile/developer-pic-2.jpg"
 import { useEffect, useRef } from "react";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/components/Skills";
 
 const AnimatedNumbers = ({value}) => {
   const ref = useRef(null)
@@ -83,6 +84,7 @@ const about = () => {
               </div>
             </div>
           </div>
+          <Skills />
         </Layout>
       </main>
     </>
